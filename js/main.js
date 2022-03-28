24f15d3d3ff5e57b8235058cf8aa895a278fad42
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-03-28 12:48:15 -0400

    Working on modals

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 "use strict";
 
+// VARIABLES AND DEFAULTS
+
+let navDev; // nav dev modal
+let navContact; // nav contact modal
+
+
+// domListeners();
+
+
+// LISTENERS
+
+navDev = function () {
+    // console.log(`click test`);
+    document.querySelector('#container-nav-modal-1-dev').classList.remove('hidden');
+    document.querySelector('#container-nav-model-3-contact').classList.add('hidden');
+}
+document.querySelector('#nav-li-id-1-dev').addEventListener('click', navDev);
+
+navContact = function () {
+    // console.log(`click test`);
+    document.querySelector('#container-nav-model-3-contact').classList.remove('hidden');
+    document.querySelector('#container-nav-modal-1-dev').classList.add('hidden');
+}
+document.querySelector('#nav-li-id-3-contact').addEventListener('click', navContact);
+
 
